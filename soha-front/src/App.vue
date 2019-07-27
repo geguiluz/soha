f1f515ee89af3047849a0ea4bac8a7320a3a30ad
geguiluz <memo.eguiluz@gmail.com>
2019-07-27 15:30:29 -0500

    Login screen frontend

---
+++ soha-front/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app dark>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>SoHa</v-toolbar-title>
+      
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ soha-front/src/components/Signup.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-container
+    fluid
+    fill-height
+    >
+    <v-layout align-center justify-center>
+        <v-flex xs12 sm8 md4>
+        <v-card class="elevation-12">
+            <v-toolbar dark flat>
+                <v-toolbar-title>Signup</v-toolbar-title>
+                <v-spacer></v-spacer>
+            </v-toolbar>
+            <v-card-text>
+            <v-form>
+                <v-text-field
+                label="E-mail"
+                name="email"
+                type="text"
+                ></v-text-field>
+
+                <v-text-field
+                id="password"
+                label="Password"
+                name="password"
+                type="password"
+                ></v-text-field>
+            </v-form>
+            </v-card-text>
+            <v-card-actions>            
+            Already a user? 
+            <v-btn color="primary" text >Login</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn color="primary">Signup</v-btn>
+            </v-card-actions>
+        </v-card>
+        </v-flex>
+    </v-layout>
+    </v-container>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
