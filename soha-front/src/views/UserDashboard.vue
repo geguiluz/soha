3b2f3104323c59d62c2fbaf31280e852719e9293
geguiluz <memo.eguiluz@gmail.com>
2019-07-29 23:25:11 -0500

    Haciendo cambios a los archivos del front

---
+++ soha-front/src/views/UserDashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h3>
+        This is the Dashboard page
+        </h3>
+        <TaskList listType = 'misTareas'/>
+        <!-- <TaskList listType = 'meDeben'/>
+        <TaskList /> -->
+    </div>
+</template>
+
+<script>
+
+import Signup from '../components/Signup';
+import TaskList from '../components/TaskList';
+
+export default {
+    components: {
+    Signup,
+    TaskList,
+  },
+}
+</script>
