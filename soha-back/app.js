a3f0b10bbb700af4bcc7f81e6385ac66151e1125
YERIM <mac@macs-MacBook-Pro.local>
2019-07-30 20:34:20 -0500

    Agrege los npm que faltaban

--- soha-back/app.js
+++ soha-back/app.js
@@ -1,4 +1,7 @@
 // CONFIG
+
+const bodyParser    = require('body-parser');
+const cookieParser  = require('cookie-parser');
 const express       = require('express');
 const mongoose      = require('mongoose');
 const session       = require("express-session");
@@ -6,9 +9,11 @@ const MongoStore    = require("connect-mongo")(session);
 const cors          = require('cors')
 const passport      = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+const logger        = require('morgan');
+const path          = require('path');
 
-const app = express();
 
+//ROUTES
 const auth     = require('./routes/auth');
 const company = require('./routes/company') 
 
@@ -18,11 +23,28 @@ mongoose.connect("mongodb://localhost:27017/soha-database",{ useNewUrlParser: tr
   })
   .catch(err => {console.error("Error connecting to mongo", err);});
 
+  const app_name = require('./package.json').name;
+  const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
+
+  const app = express();
 
 // MIDDLEWARES
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use(require('node-sass-middleware')({
+  src:  path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  sourceMap: true
+}));
+
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 // SESSION'S!
 app.use(session({
