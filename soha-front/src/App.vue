74786e7d2beb07c1e2aae4d58965d45c6b96795e
geguiluz <memo.eguiluz@gmail.com>
2019-08-02 15:31:26 -0500

    Added Navigation drawer skeleton

--- soha-front/src/App.vue
+++ soha-front/src/App.vue
@@ -11,22 +11,91 @@
       </v-btn>
     </v-app-bar>
 
+
     <v-content>
-      <router-view></router-view>
+      <v-navigation-drawer
+      absolute
+      expand-on-hover
+      permanent
+      >
+        <template v-slot:prepend>
+          <v-list>
+            <v-list-item>
+              <v-list-item-avatar>
+                <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+
+            <v-list-item
+              link
+              two-line
+            >
+              <v-list-item-content>
+                <v-list-item-title class="title">User name</v-list-item-title>
+                <v-list-item-subtitle>user_email@gmail.com</v-list-item-subtitle>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-icon>mdi-menu-down</v-icon>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </template>
+
+        <v-divider></v-divider>
+
+        <v-list
+          nav
+          dense
+        >
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-folder</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>My Files</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-account-multiple</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Shared with me</v-list-item-title>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-icon>
+              <v-icon>mdi-star</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Starred</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-container class="router-view-container">
+        <v-layout align-space-around justify-center fill-height row>
+          <v-flex>
+            <router-view></router-view>
+
+          </v-flex>
+          
+        </v-layout>
+      </v-container>
     </v-content>
+
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    
   },
   data: () => ({
     //
   }),
 };
 </script>
+
+<style scoped>
+  .router-view-container {
+    margin-left: 6vw;
+  }
+</style>
